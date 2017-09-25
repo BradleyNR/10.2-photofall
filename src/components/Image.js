fcbc926e589b0490ff7317156f8b5e85f383cc47
Bradley_Carson <bmcarson1671@gmail.com>
2017-09-25 14:29:10 -0400

    finished

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/App.css';
+import Modal from 'react-modal';
 
 class ImageList extends React.Component{
   render(){
@@ -98,7 +99,8 @@ class ImageBoard extends React.Component{
   constructor(){
     super();
     this.state = {
-      post: []
+      post: [],
+      modalOpen: false
     };
   }
 
@@ -127,16 +129,16 @@ class ImageBoard extends React.Component{
     e.preventDefault();
 
     let postList = this.state.post;
-    console.log('postlist', postList);
-
-    function findById(postList){
-      return postList._id === uniqueId;
+    // function to use in the .find below to find the correct post based on
+    // example in MDN docs
+    function findById(post){
+      return post._id === uniqueId
     }
+    // post to edit
+    let thisPost = postList.find(findById);
+    console.log(thisPost);
 
-    let postToEdit = findById(postList)
-
-    console.log('post to edit: ', postToEdit);
-
+    return this.setState({modalOpen: true, imgURL: thisPost.imgURL, caption: thisPost.caption, idToEdit: thisPost._id});
 
     // fetch('https://tiny-lasagna-server.herokuapp.com/collections/imageBoardBrad2/' + uniqueId, {
     //   method: 'put',
@@ -147,6 +149,51 @@ class ImageBoard extends React.Component{
     //   }
     // })
 
+  }
+
+  handleUrlUpdate = (e) => {
+    e.preventDefault();
+
+    this.setState({imgURL: e.target.value})
+  }
+  //handles Caption field state
+  handleCaptionUpdate = (e) => {
+    e.preventDefault();
+
+    this.setState({caption: e.target.value})
+  }
+
+  submitEdit = (e) => {
+    e.preventDefault();
+
+    let data = {
+      imgURL: this.state.imgURL,
+      caption: this.state.caption
+    }
+
+    let uniquePostUrl = this.state.idToEdit;
+
+    fetch('https://tiny-lasagna-server.herokuapp.com/collections/imageBoardBrad2/' + uniquePostUrl, {
+      method: 'put',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(()=> {
+      let postsList = this.state.post;
+
+      //find post by it's unique id
+      function findById(posts){
+        return posts._id === uniquePostUrl;
+      }
+      //set index to the index number of the post with that unique id
+      let index = postsList.indexOf(postsList.find(findById));
+      postsList[index] = data;
+      this.setState({post: postsList})
+    });
+
+    this.setState({modalOpen: false});
   }
 
   // --- removes post ---
@@ -171,6 +218,12 @@ class ImageBoard extends React.Component{
     })
   }
 
+  closeWindow = (e) => {
+    e.preventDefault();
+
+    return this.setState({modalOpen: false})
+  }
+
   // ------ GETTING DATA FROM SERVER, SETTING STATE ------
   componentDidMount(){
     fetch("https://tiny-lasagna-server.herokuapp.com/collections/imageBoardBrad2/").then((results) => {
@@ -192,6 +245,40 @@ class ImageBoard extends React.Component{
       <div>
 
         <ImageForm addPost={this.addPost} />
+
+        <Modal
+          isOpen={this.state.modalOpen}
+          shouldCloseOnOverlayClick={false}
+          contentLabel="No Overlay Click Modal"
+          className='modal-content'>
+
+          <h1 className='modal-title'>Edit Post</h1>
+          <form onSubmit={this.submitEdit} className="form-horizontal">
+
+            <div className="form-group">
+              <label className="control-label col-sm-3" htmlFor="imageUrl">Image URL:</label>
+              <div className="col-sm-7">
+                <input onChange={this.handleUrlUpdate} type="text" className="form-control" id="imageUrl" value={this.state.imgURL} placeholder="Image URL" />
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label className="control-label col-sm-3" htmlFor="caption">Caption:</label>
+              <div className="col-sm-7">
+                <input onChange={this.handleCaptionUpdate} type="text" className="form-control" id="caption" value={this.state.caption} placeholder="Caption" />
+              </div>
+            </div>
+
+            <div className="form-group">
+              <div className="col-sm-offset-8 col-sm-3">
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <button onClick={this.closeWindow} className="btn btn-danger">Close</button>
+              </div>
+            </div>
+
+          </form>
+
+        </Modal>
 
         <div className='well main-content'>
           <h1 className="main-title">PhotoFall</h1>
